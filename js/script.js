4f0a776fb311336beb6c4bc9a5afaff658de9a69
Patrik Kopáč <kopacpatrik@email.cz>
2024-02-08 17:40:39 +0100

    scrollup button hotový

--- js/script.js
+++ js/script.js
@@ -26,28 +26,50 @@ menuIcon.addEventListener(`click`, () => {
 // ----------DARK vs LIGHT MODE ------------
 
 //vyskakovacie okno - dark/light mode?
-const mode = prompt("Dark / Light mode?");
+// const mode = prompt("Dark / Light mode?");
 
 //vytiahnutie elementov z html
-const body = document.querySelector("body");
-const aboutUs = document.querySelector("#about-us");
-const ourWork = document.querySelector("#our-work");
-const ourWorkTable = document.querySelector("#our-work table");
-const footer = document.querySelector("#footer");
-const contacts = document.querySelector("#contacts");
+// const body = document.querySelector("body");
+// const aboutUs = document.querySelector("#about-us");
+// const ourWork = document.querySelector("#our-work");
+// const ourWorkTable = document.querySelector("#our-work table");
+// const footer = document.querySelector("#footer");
+// const contacts = document.querySelector("#contacts");
 
 //IF ELSE
-if (mode === "dark" || mode === "dark mode") {
-    body.style.backgroundColor = "black";
-    body.style.color = "white";
-    aboutUs.style.color = "white";
-    ourWork.style.color = "white";
-    ourWorkTable.style.color = "black";
-    footer.style.backgroundColor = "grey";
-    contacts.style.border = "1px solid white";
-} else if (mode === "light" || mode === "light mode") {
-    body.style.backgroundColor = "white";
-    body.style.color = "black";
-} else {
-    prompt("Zadajte prosím znova, či chcete tmavý alebo svetlý režim webu zadaním buď `dark` alebo `light`");
+// if (mode === "dark" || mode === "dark mode") {
+//     body.style.backgroundColor = "black";
+//     body.style.color = "white";
+//     aboutUs.style.color = "white";
+//     ourWork.style.color = "white";
+//     ourWorkTable.style.color = "black";
+//     footer.style.backgroundColor = "grey";
+//     contacts.style.border = "1px solid white";
+// } else if (mode === "light" || mode === "light mode") {
+//     body.style.backgroundColor = "white";
+//     body.style.color = "black";
+// } else {
+//     prompt("Zadajte prosím znova, či chcete tmavý alebo svetlý režim webu zadaním buď `dark` alebo `light`");
+// }
+
+
+// -------- SCROLL UP BUTTON -----------
+
+//vytiahnutie elementov z html
+const myBtn = document.querySelector("#myBtn")
+
+// funkcia - po odscrollovaní 1200px zhora webu sa užívateľovi zobrazí button
+window.addEventListener(`scroll`, () => {
+    if (window.scrollY >= 1200) {
+         myBtn.style.display = "block";
+    } else {
+         myBtn.style.display = "none";
+    }
+ })
+
+// funkcia - keď užívateľ klikne na button, automaticky ho scrollne na vrch webu
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
+
