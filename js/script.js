9e9a88fd148d0ee5bb285ca0d76df9f7f9ccd218
Patrik Kopáč <kopacpatrik@email.cz>
2024-02-08 15:59:02 +0100

    upravené js súbory

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
+// ------HAMBURGER MENU--------
 
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    initialSlide: 1,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
+// tvorba premenných
+const menuIcon = document.querySelector(`.menu-icon`)
+
+const menuList = document.querySelector("nav")
+
+const hamburgerIcon = document.querySelector(".fa-solid")
+
+// prepínanie classov z hamburger menu na krížik a naopak
+menuIcon.addEventListener(`click`, () => {
+    if (hamburgerIcon.classList[1] === "fa-bars") {
+        hamburgerIcon.classList.toggle("fa-xmark")
+        hamburgerIcon.classList.remove("fa-bars")
+        menuList.style.display = "block";
+    } else {
+        hamburgerIcon.classList.add("fa-bars")
+        hamburgerIcon.classList.remove("fa-xmark");
+        menuList.style.display = "none"
+    }
+})
--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,23 @@
-/*--------- Initialize Swiper --------*/
 
 var swiper = new Swiper(".mySwiper", {
-      navigation: {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    initialSlide: 1,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
-      },
   });
-
