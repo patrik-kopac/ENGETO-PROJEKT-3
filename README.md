94290ba3af8c483f046400f6bc930f7404f01bee
Patrik Kopáč <kopacpatrik@email.cz>
2024-02-08 15:48:15 +0100

    skopírovanie webu klienta pre projekt engeto

---
+++ README.md
@@ -0,0 +1,5 @@
+Vytvoril som webovú prezentáciu pre projektového manažera a projektanta fotovoltaických elektráreň Ing. Jiřího Londýna z Uherského Hradišťa. 
+
+Zadanie bolo minimalizmus, jednoduchosť, čierno-šedo-biely vzhľad a aby vynikli fotografie projektov, čož sa verím po pár úpravách celkom podarilo. 
+
+Použité boli kódy v HTML, CSS a niečo málo z JavaScriptu (viac zatiaľ nebolo treba). 
---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+/*--------- Initialize Swiper --------*/
+
+var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+  });
+
